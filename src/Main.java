98be1238e1a971f65a48e4b9ebb9aef8f72dfe3e
Igoryok8 <energymag@mail.ru>
2022-10-18 23:49:38 +0300

    Задание 1 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Задание 1");
+        System.out.println();
+        String firstName = "Иван ";
+        String middleName = "Иванович ";
+        String lastName = "Иванов ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " +fullName);
     }
 }
