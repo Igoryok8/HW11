d3c9e7632009c86cdb6ecee9fec7fe4ffbfa17d0
Igoryok8 <energymag@mail.ru>
2022-10-19 00:28:19 +0300

    Задание 2 выполнено

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,11 @@ public class Main {
         String lastName = "Иванов ";
         String fullName = lastName + firstName + middleName;
         System.out.println("ФИО сотрудника — " +fullName);
+        System.out.println();
+        System.out.println("Задание 2");
+        System.out.println();
+        fullName = lastName + firstName + middleName;
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " +fullName1);
     }
 }
